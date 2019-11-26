f3b2b3c27144e520ad5da073bfcd3ca2af2a1b3b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-11-26 10:22:48 +0700

    tipe data enum

---
+++ tipedata-enum.ts
@@ -0,0 +1,14 @@
+enum Gender { Male = 1, Female = 2, Unknown = -1 }
+
+let jenisKelamin: Gender = Gender.Male;
+
+console.log(jenisKelamin);
+
+enum Category {
+    Grocery = "GROCERY" ,
+    Electronic = "ELECTRONIC",
+    Handphone = "HANDPHONE"
+}
+
+let kategory: Category = Category.Handphone;
+console.log(kategory);
